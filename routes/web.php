75f68751037b1e4d2d9342d879f7a98f910b8c8b
Camilo <paula@pena-parra.ch>
2026-05-22 14:53:18 +0200

    initialised project stack : Laravel + Vue + Inertia + Precognition + shadcn

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::inertia('/', 'Welcome')->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::inertia('dashboard', 'Dashboard')->name('dashboard');
+});

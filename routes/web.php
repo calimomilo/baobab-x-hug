b0bc28c558e3583f8a846ad600ef6f1f5d9de50c
Camilo <paula@pena-parra.ch>
2026-05-31 12:06:27 +0200

    added routes for existing controllers

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,23 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CollectController;
+use App\Http\Controllers\CompanyController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'Welcome')->name('home');
 
-Route::middleware(['auth'])->group(function () {
-    Route::inertia('dashboard', 'Dashboard')->name('dashboard');
+Route::controller(AuthController::class)->group(function () {
+    Route::get('/auth/login', 'showLogin')->name('login');
+    Route::post('/auth/login', 'login');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::resource('companies', CompanyController::class);
+
+    Route::resource('collects', CollectController::class);
+    Route::put('/collects/{id}/complete', [CollectController::class, 'complete']);
+    Route::put('/collects/{id}/incomplete', [CollectController::class, 'incomplete']);
+
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
 });

d31f8b3e45d51a25c712d2ccbffd7f655a52c316
Camilo <paula@pena-parra.ch>
2026-06-02 10:45:51 +0200

    feature: desktop header and menu

---
+++ resources/js/components/MenuButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
+
+const props = defineProps({
+    href : {type: String, default: ''},
+    selected : {type: Boolean, default: false},
+    type : {type: String, default: 'normal'}
+})
+</script>
+
+<template>
+    <Link :href="props.href" 
+        class="px-3 py-2 rounded font-medium"
+        :class="{'bg-brand-sage-100' : props.selected, 
+            'text-brand-sage-950 hover:bg-white/60 active:bg-white/80' : props.type==='normal',
+            'text-white bg-brand-rose-400 hover:bg-brand-rose-500/100 active:bg-brand-rose-600/100' : props.type==='highlight-pink'}">
+        <slot />
+        <!-- <hr v-if="props.selected" class="border-2 mx-1 rounded-full"> -->
+    </Link>
+</template>
--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
+import MenuButton from '@/components/MenuButton.vue';
 
 const props = defineProps({
     title : {type: String, default: 'HUG Blood League'},
@@ -15,12 +16,34 @@ const props = defineProps({
         <link rel="preconnect" href="https://rsms.me/" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     </Head>
-    <div class="flex min-h-screen flex-col">
+    <div class="flex min-h-screen flex-col font-cooper">
         <header class="bg-brand-sage-300 flex p-3 justify-between items-center">
-            <div class="flex gap-3 items-center text-white">
+            <Link href="/" class="flex gap-3 items-center text-white">
                 <img src="../../assets/BloodLeague_logo_blanc_filled.png" alt="Logo Blood League" class="h-10 lg:h-12">
                 <span class="hidden lg:inline">✕</span>
                 <img src="../../assets/LOG HUG_H_NEGATIF fond transparent.png" alt="Logo HUG" class="h-10 hidden lg:inline">
+            </Link>
+            <!-- <NavigationMenu>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink as-child :class="navigationMenuTriggerStyle()">
+                            <a href="/leaderboard">LeaderBoard</a>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuTrigger>En savoir plus</NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                            <a href="/blood-league" :class="navigationMenuTriggerStyle()" class="bg-transparent">Qu'est-ce que la Blood League ?</a>
+                            <a href="/don-du-sang" :class="navigationMenuTriggerStyle()" class="bg-transparent">Pourquoi donner ?</a>
+                        </NavigationMenuContent>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu> -->
+            <div class="hidden lg:flex gap-2">
+                <MenuButton href="/leaderboard" selected>Leaderboard</MenuButton>
+                <MenuButton href="/blood-league">La Blood League</MenuButton>
+                <MenuButton href="/don-du-sang">Pourquoi donner ?</MenuButton>
+                <MenuButton href="/contact" type="highlight-pink">Organiser une collecte</MenuButton>
             </div>
         </header>
         <slot />

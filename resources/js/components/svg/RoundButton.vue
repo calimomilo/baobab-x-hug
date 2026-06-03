3dabadd5d55c789c92a407a930ce1f9bfd689714
Camilo <paula@pena-parra.ch>
2026-06-03 07:38:18 +0200

    feature: tests for making a component from the round button

---
+++ resources/js/components/svg/RoundButton.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+const props = defineProps({
+    id : {type: String, default: 'id'},
+    src : {type: String, default: null},
+    alt : {type: String, default: ''},
+    background : {type: String, default: '#000000'}
+})
+
+const svg = document.querySelector(`#${props.id}`);
+svg?.setAttribute('fill', props.background);
+</script>
+
+<template>
+    <img v-if="props.src" :id="props.id" :src="props.src" :alt="props.alt">
+</template>

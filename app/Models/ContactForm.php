72188f51298f16a71cb118e0536659ec3b575326
Camilo <paula@pena-parra.ch>
2026-06-05 16:53:15 +0200

    fix: added fillable on models

--- app/Models/ContactForm.php
+++ app/Models/ContactForm.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 
+#[Fillable(['company_name', 'address', 'contact_address', 'contact_name', 'email', 'phone'])]
 class ContactForm extends Model
 {
     //

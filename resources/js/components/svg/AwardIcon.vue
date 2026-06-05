4a04e1485c7af34f6d29e59ac1d252c1019e6b89
Camilo <paula@pena-parra.ch>
2026-06-05 07:16:23 +0200

    fix: image refs

--- resources/js/components/svg/AwardIcon.vue
+++ resources/js/components/svg/AwardIcon.vue
@@ -8,9 +8,9 @@ const props = defineProps({
 </script>
 
 <template>
-    <img v-if="props.award === 'climber'" src="../../../assets/awards/The_Climber.svg" alt="" :class="props.class">
-    <img v-if="props.award === 'flood'" src="../../../assets/awards/The_Flood.svg" alt="" :class="props.class">
-    <img v-if="props.award === 'golden_heart'" src="../../../assets/awards/The_Golden_Heart.svg" alt="" :class="props.class">
-    <img v-if="props.award === 'new_vein'" src="../../../assets/awards/The_New_Vein.svg" alt="" :class="props.class">
-    <img v-if="props.award === 'pulse'" src="../../../assets/awards/The_Pulse.svg" alt="" :class="props.class">
+    <img v-if="props.award === 'climber'" src="/assets/awards/The_Climber.svg" alt="" :class="props.class">
+    <img v-if="props.award === 'flood'" src="/assets/awards/The_Flood.svg" alt="" :class="props.class">
+    <img v-if="props.award === 'golden_heart'" src="/assets/awards/The_Golden_Heart.svg" alt="" :class="props.class">
+    <img v-if="props.award === 'new_vein'" src="/assets/awards/The_New_Vein.svg" alt="" :class="props.class">
+    <img v-if="props.award === 'pulse'" src="/assets/awards/The_Pulse.svg" alt="" :class="props.class">
 </template>

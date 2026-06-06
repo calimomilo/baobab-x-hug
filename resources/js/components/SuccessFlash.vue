6594ed8be46546f5177773e6b002566daa0bfad4
Camilo <paula@pena-parra.ch>
2026-06-06 06:31:32 +0200

    fix: detail

--- resources/js/components/SuccessFlash.vue
+++ resources/js/components/SuccessFlash.vue
@@ -1,11 +1,11 @@
-<!-- <script setup lang="ts">
-import { usePage } from '@inertiajs/vue3';
+<script setup lang="ts">
+// import { usePage } from '@inertiajs/vue3';
 
-const page = usePage();
+// const page = usePage();
 
 </script>
 
-<template>
+<!-- <template>
     <div id="toast" v-if="page.flash.toast" class="min-w-100 px-8 py-6 text-sm font-medium fixed z-20 top-10 right-10 rounded shadow-lg" :class="{
         'bg-lime-200 text-lime-900': page.flash.toast.success,
         'bg-brand-error-300 text-brand-error-950': !page.flash.toast.success

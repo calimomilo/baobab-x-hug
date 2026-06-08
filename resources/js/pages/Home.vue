e567ae186c7ffe1d8af7bae46bd15cee4e01ad2f
Camilo <60544411+calimomilo@users.noreply.github.com>
2026-06-08 14:25:24 +0200

    Update resources/js/pages/Home.vue
    Co-authored-by: Thierry Koulbanis <thierrykoulba@gmail.com>

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -44,7 +44,7 @@ props.companies?.forEach((company) => {
     <AppLayout title="Accueil" desc="Bienvenue sur le site de la Blood League par les HUG">
         <section id="hero" class="relative min-h-[calc(100vh-76px)] flex flex-col justify-center items-center gap-20 md:gap-6">
             <h1 class="flex flex-col text-center text-2xl font-bold gap-2 items-center md:text-4xl md:gap-3 lg:text-[64px] lg:gap-6">
-                <span class="-rotate-6 -translate-x-[calc(10vw)] md:-translate-x-[calc(20vw-100px)] lg:-translate-x-[calc(20vw-150px)">Mobilisez votre équipe.</span>
+                <span class="-rotate-6 -translate-x-[calc(10vw)] md:-translate-x-[calc(20vw-100px)] lg:-translate-x-[calc(20vw-150px)]">Mobilisez votre équipe.</span>
                 <span class="rotate-4 translate-x-[calc(10vw+15px)] lg:translate-x-[calc(25vw-80px)] lg:translate-y-2">Sauvez des vies.</span>
                 <span>Rejoignez la Blood League.</span>
             </h1>

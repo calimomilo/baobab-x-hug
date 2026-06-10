0406a6c3b1ae28e564621904b8268a08127b6dcf
Camilo <paula@pena-parra.ch>
2026-06-10 22:37:33 +0200

    feature: favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,9 +30,9 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" href="/assets/logos/BloodLeague_logo_simple.png" sizes="any">
+        {{-- <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png"> --}}
 
         @fonts
 
